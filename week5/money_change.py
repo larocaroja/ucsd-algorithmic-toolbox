5aa671ee0e5a8fbde2e1202381a77cad75dcd82d
Hongseok Oh <49251855+larocaroja@users.noreply.github.com>
2023-08-25 05:25:35 +0900

    add exception

--- week5/money_change.py
+++ week5/money_change.py
@@ -1,17 +1,17 @@
 import math
 
-N_COINS = dict()
-N_COINS[0] = 0
-denominators = [1, 3, 4]
-
 def money_change_dp(money):
+    N_COINS = [0 if v == 0 else math.inf for v in range(money+1)]
+    denominators = [1, 3, 4]
+    
     for i in range(1, money+1):
-        N_COINS[i] = math.inf
-
         for denominator in denominators:
             if denominator <= i:
                 if N_COINS[i-denominator] + 1 < N_COINS[i]:
                     N_COINS[i] = N_COINS[i-denominator] + 1
+
+    if N_COINS[money] == math.inf:
+        return -1
 
     return N_COINS[money]
 
